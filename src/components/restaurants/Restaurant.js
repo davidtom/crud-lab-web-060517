ec4f4e42605c2d1adf2e4f45d6634543fcf47979
davidtom <davidjtomczyk@gmail.com>
2017-08-26 21:42:20 -0400

    pass restaurant tests

--- src/components/restaurants/Restaurant.js
+++ src/components/restaurants/Restaurant.js
@@ -1,9 +1,19 @@
 import React, { Component } from 'react';
 
 class Restaurant extends Component {
+
+  handleOnClick(event){
+    this.props.store.dispatch({
+      type: "DELETE_RESTAURANT",
+      id: this.props.restaurant.id
+    })
+  }
+
   render() {
     return (
       <div>
+        <li>{this.props.restaurant.text}</li>
+        <button onClick={(event)=>this.handleOnClick(event)}>X</button>
       </div>
     );
   }
--- src/reducers/manageRestaurant.js
+++ src/reducers/manageRestaurant.js
@@ -2,6 +2,15 @@
 import cuid from 'cuid';
 export const cuidFn = cuid;
 
-export default function manageRestaurants(state, action) {
-
+export default function manageRestaurants(state = {restaurants: []}, action) {
+  switch (action.type){
+    case "ADD_RESTAURANT":
+      let id = cuidFn()
+      return {restaurants: state.restaurants.concat({text: action.restaurant.text, id: id})}
+    case "DELETE_RESTAURANT":
+      const restaurants = state.restaurants.filter(restaurant => restaurant.id !== action.id)
+      return {restaurants}
+    default:
+      return state;
+  }
 }
